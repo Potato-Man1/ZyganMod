2471bfdcad166717a7239c7088514f8a76a68e29
Potato-Man1 <89878281+Potato-Man1@users.noreply.github.com>
2021-09-01 20:29:37 +0800

    Update ZyganBlocks.java

--- src/zygan/content/ZyganBlocks.java
+++ src/zygan/content/ZyganBlocks.java
@@ -44,37 +44,25 @@ public class ZyganBlocks implements ContentList {
             rotateSpeed = 25f;
         }};
 	    
-	repeater = new PowerTurret("Repeater"){{
-            requirements(Category.turret, with(Items.copper, 60, Items.lead, 70, Items.silicon, 50));
-            range = 165f;
-            chargeTime = 2f;
-            chargeMaxDelay = 15f;
-            chargeEffects = 7;
-            recoilAmount = 0.1f;
-            reloadTime = 40f;
-            cooldown = 0.03f;
-            powerUse = 6f;
-            shootShake = 2f;
-            shootEffect = Fx.lancerLaserShoot;
-            smokeEffect = Fx.none;
-            chargeEffect = Fx.lancerLaserCharge;
-            chargeBeginEffect = Fx.lancerLaserChargeBegin;
-            heatColor = Color.red;
-            size = 2;
-            health = 280 * size * size;
-            targetAir = false;
-            shootSound = Sounds.laser;
-
-            shootType = new LaserBulletType(140){{
-                colors = new Color[]{Pal.lancerLaser.cpy().a(0.4f), Pal.lancerLaser, Color.white};
-                hitEffect = Fx.hitLancer;
-                hitSize = 2;
-                lifetime = 16f;
-                drawSize = 400f;
-                collidesAir = true;
-                length = 300f;
-                ammoMultiplier = 1f;
-            }};
-        }}; 
+       makria = new ItemTurret("makria"){{
+	    requirements(Category.turret, with(Items.copper, 35), true);
+	    ammo(
+                Items.copper, Bullets.standardCopper,
+                Items.graphite, Bullets.standardDense,
+                Items.pyratite, Bullets.standardIncendiary,
+                Items.silicon, Bullets.standardHoming
+            );
+	    spread = 1f;
+            shots = 1;
+            alternate = true;
+            reloadTime = 80f;
+            restitution = 0.03f;
+            range = 500;
+            shootCone = 9f;
+            ammoUseEffect = Fx.casing1;
+            health = 200;
+            inaccuracy = 0.0001f;
+            rotateSpeed = 5f;
+        }}
     }
 }
