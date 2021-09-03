bcccaf724fa7cbdf4a17276bfa273ae238148528
Potato-Man1 <89878281+Potato-Man1@users.noreply.github.com>
2021-09-03 18:10:20 +0800

    Update ChainTurret.java

--- src/zygan/world/blocks/defense/turrets/ChainTurret.java
+++ src/zygan/world/blocks/defense/turrets/ChainTurret.java
@@ -1,1 +1,66 @@
+package zygan.world.blocks.defense.turrets;
 
+import arc.struct.*;
+import mindustry.entities.bullet.*;
+import mindustry.logic.*;
+import mindustry.world.meta.*;
+
+public class ChainTurret extends Turret{
+    public BulletType shootType;
+    public float powerUse = 1f;
+    public float chainRange = 300f;
+
+    public ChainTurret(String name){
+        super(name);
+        hasPower = true;
+    }
+
+    @Override
+    public void setStats(){
+        super.setStats();
+        stats.add(Stat.ammo, StatValues.ammo(ObjectMap.of(this, shootType)));
+    }
+
+    @Override
+    public void init(){
+        consumes.powerCond(powerUse, TurretBuild::isActive);
+        super.init();
+    }
+
+    public class ChainTurretBuild extends TurretBuild{
+
+        @Override
+        public void updateTile(){
+            if(unit != null){
+                unit.ammo(power.status * unit.type().ammoCapacity);
+            }
+
+            super.updateTile();
+        }
+
+        @Override
+        public double sense(LAccess sensor){
+            return switch(sensor){
+                case ammo -> power.status;
+                case ammoCapacity -> 1;
+                default -> super.sense(sensor);
+            };
+        }
+
+        @Override
+        public BulletType useAmmo(){
+            return shootType;
+        }
+
+        @Override
+        public boolean hasAmmo(){
+            return true;
+        }
+
+        @Override
+        public BulletType peekAmmo(){
+            return shootType;
+        }
+        
+    }
+}
